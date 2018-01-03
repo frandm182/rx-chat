a102338ef48770f71064edf52dbd92c018718e84
Francisco Diaz <franciscojavier.diaz@i3television.es>
2018-01-03 12:56:51 +0100

    Adding socket functionallity

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const http = require('http')
 const bodyParser = require('body-parser')
 const socketio = require('socket.io')
+const Immutable = require('immutable')
+const { Observable } = require('rxjs')
 
 //Initialize aplication
 const app = express()
@@ -15,7 +17,24 @@ const server = http.createServer(app)
 //Initialize socket io with server
 const io = socketio(server)
 
+const usersList = Immutable.Map({});
+
 //socket logic on connect
+
+const sourceConnect = Observable.create(observer => {
+    io.on('connection', socket => {
+        socket.emit('my socketId', { socketId: socket.id, connectTime: Date.now() })
+        socket.on('client connect', data => {
+            observer.next({ socket: socket, data: data, event: 'client connect' })
+        })
+    })
+    
+})
+
+sourceConnect.subscribe(obj => {
+    userList.set(obj.data.socketId, obj.data)
+    io.emit('all users', userList.toArray())
+})
 
 //socket logic for post message
 app.post('/message', (req,res) => {
@@ -23,7 +42,18 @@ app.post('/message', (req,res) => {
 })
 
 //socket logic on disconnect
+const sourceDisconnect = Observable.create(observer => {
+     io.on('connection', socket => {         
+       socket.on('client connect', data => {
+        observer.next({ socketId: socket.id, event: 'client disconnect' })
+       })
+    })
+})
 
+sourceDisconnect.subscribe(obj => {
+    userList.delete(obj.socketId)
+    io.emit('all users', userList.toArray())
+})
 
 //http://localhost:4000
 app.get('/', (req,res) => {
